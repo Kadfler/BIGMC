f588c9db0e656a2fd185df98e82e386cd5c1ceee
KADFLER <mgav4751@Gmail.com>
2025-05-13 20:38:17 +0300

    адаптив первой страницы часть 1

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,18 +1,37 @@
 new Swiper('.swiper-container')
 
 const swiper = new Swiper('.swiper', {
-    loop: true,
-    direction: "vertical",
-    spaceBetween:20,
+    breakpoints: {
+        800: {
+            loop: true,
+            direction: "vertical",
+            spaceBetween: 25,
 
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable:true,
-    },
-  
-    mousewheel: {
-        invert: false,
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'bullets',
+                clickable: true,
+            },
+
+            mousewheel: {
+                invert: false,
+            }
+        },
+        1920: {
+            loop: true,
+            direction: "horizontal",
+            spaceBetween: 25,
+
+            pagination: {
+                el: '.swiper-pagination',
+                type: 'bullets',
+                clickable: true,
+            },
+
+            mousewheel: {
+                invert: false,
+            }
+        }
     }
+    });
 
-  });
